1c4fda5acb7d04412da86923283fd9a33416e262
PooriaT <pooria_taghdiri@hotmail.com>
2024-01-10 09:36:23 -0800

    minor changes

--- core/utils/process.py
+++ core/utils/process.py
@@ -12,12 +12,12 @@ def process_podcast_text(podcast_name: str, episode_name: str) -> None:
     if podcast_url:
         episode_audio_url, episode_duration = get_episode_audio_url(podcast_url, episode_name)
         if episode_audio_url and "Failed to fetch data" not in episode_audio_url:
-            # download_episode_audio(episode_audio_url)
-            # print(f"You need to be patient. It takes time to process the audio. Please wait approximately for {int(episode_duration * 0.33)} minutes.")
-            # os.makedirs(BASE_TEXT_PATH, exist_ok=True)
-            # whole_text = transcribe_large_audio_with_whisper(f"{BASE_AUDIO_PATH}podcast.mp3")
-            # with open(os.path.join(BASE_TEXT_PATH, "whole_text.txt"), "w", encoding="utf-8") as file:
-            #     file.write(whole_text)
+            download_episode_audio(episode_audio_url)
+            print(f"You need to be patient. It takes time to process the audio. Please wait approximately for {int(episode_duration * 0.33)} minutes.")
+            os.makedirs(BASE_TEXT_PATH, exist_ok=True)
+            whole_text = transcribe_large_audio_with_whisper(f"{BASE_AUDIO_PATH}podcast.mp3")
+            with open(os.path.join(BASE_TEXT_PATH, "whole_text.txt"), "w", encoding="utf-8") as file:
+                file.write(whole_text)
             print("Podcast Text generated successfully!")
             summary = summarizing(os.path.join(BASE_TEXT_PATH, "whole_text.txt"), streaming=False)
             return {"text": summary.text}
