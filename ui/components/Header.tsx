0f028eb6245693149c748d5ab503f408412702d6
PooriaT <pooria_taghdiri@hotmail.com>
2024-01-11 23:08:08 -0800

    main UI structure

---
+++ ui/components/Header.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Header() {
+    return (
+        <header className="top-0 left-0 right-0 h-16 bg-gray-900 text-white flex items-center justify-center shadow-md">
+            <div className="flex items-center gap-4">
+                <h1 className="text-2xl font-bold">SUMCAST</h1>
+            </div>
+        </header>
+    )
+}
